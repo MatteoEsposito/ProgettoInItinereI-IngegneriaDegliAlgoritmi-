442323bf48dfe42a1064a8bca0b3efb9573f407e
Matteo Esposito <esposito.matteo@outlook.com>
2017-12-11 10:45:36 +0100

    Update settings.py

--- Python/lib/settings.py
+++ Python/lib/settings.py
@@ -3,6 +3,6 @@
 # Autore: Matteo Esposito
 # Versione di Python: 2.6.9
 
-# Variabili Globali per impostare la verbosità dell'algoritmo e facilitarne il DEBUG ed/o il TESTING e/o il PROFILING
+# Variabili Globali per impostare la verbosità dell'algoritmo e facilitarne il DEBUG ed/o il TESTING ed/o il PROFILING
 DEBUG = False
 RELEASE = False
